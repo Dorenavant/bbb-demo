43c309b2dbf1d04ed47e7505d67ff39495a408a6
Ignatius Sinn <Ignatius>
2019-01-10 15:05:34 -0500

    Restructure functions to use before_action for API initialization

--- app/controllers/big_blue_button_controller.rb
+++ app/controllers/big_blue_button_controller.rb
@@ -1,44 +1,24 @@
 class BigBlueButtonController < ApplicationController
-  def init
-    require "bigbluebutton_api"
-
-    @api = BigBlueButton::BigBlueButtonApi.new(
-      Rails.configuration.bigbluebutton_endpoint,
-      Rails.configuration.bigbluebutton_secret,
-      Rails.configuration.bigbluebutton_version,
-      true
-    )
-
-    # Create demo meeting to join
-    @name = "Demo Meeting"
-    @id = "testID"
-    @options = {:moderatorPW => Rails.configuration.demo_moderator_pw,
-                :attendeePW => Rails.configuration.demo_attendee_pw,
-                :welcome => "Welcome to the Demo Meeting",
-                :logoutURL => "https://google.ca",
-                :record => true,
-                :maxParticipants => 25}
-
-    unless @api.is_meeting_running?(@id) # Check if meeting is running
-      @api.create_meeting(@name, @id, @options)
-    end
-  end
+  before_action :initAPI
 
   def login
+    createMeeting
   end
 
   def join
-    @join_name = params[:big_blue_button][:name]
+    @join_name = params[:big_blue_button][:name] # Instance variable so view can repopulate
     join_password = params[:big_blue_button][:password]
 
     unless @join_name == "" # Cannot join with empty name
-      init
-
-      if join_password == @options[:moderatorPW]
-        meeting_url = @api.join_meeting_url(@id, @join_name, @options[:moderatorPW])
+      if join_password == session[:moderatorPW]
+        meeting_url = @api.join_meeting_url(session[:id],
+                                            @join_name,
+                                            session[:moderatorPW])
         redirect_to meeting_url
-      elsif join_password == @options[:attendeePW]
-        meeting_url = @api.join_meeting_url(@id, @join_name, @options[:attendeePW])
+      elsif join_password == session[:attendeePW]
+        meeting_url = @api.join_meeting_url(session[:id],
+                                            @join_name,
+                                            session[:attendeePW])
         redirect_to meeting_url
       else
         @error_state = true
@@ -52,9 +32,43 @@ class BigBlueButtonController < ApplicationController
   end
 
   def get_recordings
-    init
+    @api.join_meeting_url(session[:id],
+                          @join_name,
+                          session[:attendeePW])
+    recordings = @api.get_recordings({:meetingID => session[:id]})
+    # render plain: recordings
+  end
 
-    # @api.get_meetings()
-    recordings = @api.get_recordings({:meetingID => @id})
+  private
+
+  def initAPI
+    require "bigbluebutton_api"
+
+    @api = BigBlueButton::BigBlueButtonApi.new(
+      Rails.configuration.bigbluebutton_endpoint,
+      Rails.configuration.bigbluebutton_secret,
+      Rails.configuration.bigbluebutton_version,
+      true
+    )
+  end
+
+  def createMeeting
+    # Create demo meeting to join
+    session[:name] = "Demo Meeting"
+    session[:id] = "testID"
+    session[:moderatorPW] = Rails.configuration.demo_moderator_pw
+    session[:attendeePW] = Rails.configuration.demo_moderator_pw
+    options = {:moderatorPW => session[:moderatorPW],
+               :attendeePW => session[:attendeePW],
+               :welcome => "Welcome to the Demo Meeting",
+               :logoutURL => "https://google.ca",
+               :record => true,
+               :maxParticipants => 25}
+
+    unless @api.is_meeting_running?(session[:id]) # Check if meeting is running
+      @api.create_meeting(session[:name],
+                          session[:id],
+                          options)
+    end
   end
 end
